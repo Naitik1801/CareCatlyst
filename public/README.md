2c766adf73cbac5e864ee9a87d8a7b1aa498561b
Naitik1801 <96631140+Naitik1801@users.noreply.github.com>
2024-04-04 10:41:57 +0530

    Add files via upload

---
+++ public/README.md
@@ -0,0 +1,46 @@
+Project Code: versed-substance-8213
+
+Website: https://www.ketto.org/
+
+About: Ketto is an Online Crowdfunding Platform and Website in India for fundraising of Social, Charity, Movies, Music, Personal and Creative causes.
+
+Manager/IA: Gautam Verma
+
+
+Team Lead
+fw19_0350 - Sofiyan Shaikh
+
+Member:-1
+fw20_0612 - Prabhat Poddar 
+
+Member:-2
+fw20_0423 - Pranshu Singh Bisht
+
+Member:-3
+fw20_0972 - Arun Kumar Rana
+
+Member:-4
+fw19_0813 - Kartikey Mishra
+
+
+## Visit Site :-https://musical-jalebi-5be9ac.netlify.app/
+
+
+### Technologies Used:
+
+---
+
+1. HTML
+2. CSS
+3. JavaScript
+
+#### Project Look Like 
+
+
+![Screenshot_20230112_035338](https://user-images.githubusercontent.com/98205449/212042354-44ca4a0c-c3af-4a25-a02a-1de84a9e9803.png)
+
+![Screenshot_20230112_035401](https://user-images.githubusercontent.com/98205449/212042389-70a27968-931d-491d-8205-a8aed84efd4d.png)
+![Screenshot_20230112_035431](https://user-images.githubusercontent.com/98205449/212042413-35cfe0c5-cc86-4e2a-9031-23b14a2a48b0.png)
+![Screenshot_20230112_035446](https://user-images.githubusercontent.com/98205449/212042427-933d67e4-db34-42a8-a431-279cd2e06f79.png)
+![Screenshot_20230112_035502](https://user-images.githubusercontent.com/98205449/212042458-79776294-3ca8-4ff0-82bf-0db9918d6e28.png)
+![Screenshot_20230112_035530](https://user-images.githubusercontent.com/98205449/212042476-0c9ac425-76e9-47fd-9cb9-551a4a523554.png)
---
+++ public/scripts/eduslider.js
@@ -0,0 +1,106 @@
+const sliderContainer = document.querySelector('.slider-container');
+const cardsContainer = document.querySelector('.cards');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+const body = document.querySelector("body");
+// const checkout = document.getElementsByClassName("donateBtn");
+let cardIndex = 0;
+
+function updateSliderPosition() {
+    // const cardWidth = document.querySelector('.trendCard').offsetWidth - 1; // Adjust the width and margin
+    cardsContainer.style.transform = `translateX(${-cardIndex * 340}px)`;
+}
+// function addDonate() {
+//   for (var i = 0; i < checkout.length; i++) {
+//     checkout[i].addEventListener("click", () => {
+//       $.ajax({
+//         url: "/checkout-session",
+//         type: "POST",
+//         contentType: "application/json",
+//         data: JSON.stringify({ quantity: 1 }),
+//         success: function (data, textStatus, xhr) {
+//           var stripe = Stripe(
+//             "pk_test_51P0frbSHbBcqBAz5ffHOQBtb9g6ZUeh8i8Q5ztbayzatFEsKZWjDBfD5WDjPeiSMWRjctx0SLgCwXUNIcFy151OS0017jxZPNF"
+//           );
+//           return stripe.redirectToCheckout({ sessionId: data.id });
+//         },
+//       });
+//     });
+//   }
+// }
+function showCards(data) {
+        data.forEach(item => {
+            const card = document.createElement('div');
+            card.className = 'trendCard';
+            let reach = item.a * 100 / item.t;
+            reach = reach.toFixed(0)
+            reach = +reach
+            card.innerHTML = `
+            <div class="trendImg">
+                <img class="tImg" src="${item.image}" />
+              </div>
+              <div class="title"><a href="${item.url}">${item.title}</a></div>
+              <div class="raisedBy">
+                <span>FF</span>
+                <p class="name">${item.by}</p>
+              </div>
+              <div class="raisedAmt"><p><span>${item.amount}</span> raised out of</p></div>
+              <div class="target">${item.target}</div>
+              <div class="bar">
+                <div class="achieve" style="width:${reach}%"></div>
+              </div>
+              <div class="trendDetails">
+                <div class="timeLeftWrap">
+                  <span class="clock">
+                    <img src="./Images/clockicon.png" alt="" />
+                  </span>
+                  <span class="timeLeft"> <span>${item.dayleft}</span></span>
+                  days left
+                </div>
+                <div class="supports">
+                  <span class="heart">
+                    <img src="./Images/hearticon.png" alt="" />
+                  </span>
+                  <span class="totalSupport">${item.support}</span>
+                  Supporters
+                </div>
+              </div>
+              <div class="cardBtns">
+                <button class="shareFB" onclick="window.location.href='https://www.facebook.com/sharer/sharer.php?u=https://www.example.com'">
+                  <span class="FBimg">
+                    <img src="./Images/fbicon.png" alt="FB" />
+                  </span>
+                  <span>Share</span>
+                </button>
+                <a href="donate.html"><button class="donateBtn">Donate</button></a>
+              </div>
+            </div>
+            `;
+            cardsContainer.appendChild(card);
+        })
+}
+
+fetch('http://localhost:3000/items/edu')
+    .then(response => response.json())
+    .then(data => {
+        showCards(data);
+        // addDonate();
+        const scripts = document.createElement("script");
+        scripts.src = "https://js.stripe.com/v3/";
+        body.appendChild(scripts);
+            prevBtn.addEventListener('click', () => {
+                if (cardIndex > 0) {
+                    cardIndex--;
+                    updateSliderPosition();
+                }
+            });
+    
+            nextBtn.addEventListener('click', () => {
+                if (cardIndex < data.length - 1) {
+                    cardIndex++;
+                    updateSliderPosition();
+                }
+            });;
+        }).catch(error => {
+        console.error('Error fetching data:', error);
+    });
