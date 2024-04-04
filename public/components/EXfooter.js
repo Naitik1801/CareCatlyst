5ea065d947da40fac2bbd09f9d3db03f62355931
Naitik1801 <96631140+Naitik1801@users.noreply.github.com>
2024-04-04 10:41:57 +0530

    Add files via upload

---
+++ public/components/EXfooter.js
@@ -0,0 +1,86 @@
+function footer() {
+  return ` <footer>
+  <div id="footer">
+    <div id="mainfoot">
+      <div id="foot1">
+        <div id="footimg">
+          <img src="Images/carecatalyst-high-resolution-logo-transparent.png" alt="">
+        </div>
+        <div id="footlogo">
+          <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.example.com"> <i class="fa-brands fa-facebook-f"></i></a>
+          <a href="https://twitter.com/?lang=en"> <i class="fa-brands fa-twitter"></i></a>
+          <a href="https://www.linkedin.com/"> <i class="fa-brands fa-linkedin"></i></a>
+          <a href="https://www.youtube.com/"> <i class="fa-brands fa-youtube"></i></a>
+          <a href="https://www.instagram.com/carecatalyst_org?igsh=MTd5d3J6bTN4ZHRtMg=="> <i class="fa-brands fa-instagram"></i></a>
+          <a href="https://api.whatsapp.com/send?phone=9199999-XXXXX"> <i class="fa-brands fa-whatsapp"></i></a>
+
+
+        </div>
+
+  
+
+        <div id="Footcontact">
+          <p>For any queries</p>
+          <p>Email: info@carecataylst.org</p>
+          <p> Contact No: +91 99999-XXXXX</p>
+
+        </div>
+      </div>
+      <div id="foot2">
+        <h2>Cause</h2>
+        <div>
+              <a href="medical.html" style="text-decoration: none; color: white; ;">Medical crowdfunding</a>
+              <a href="children.html" style="text-decoration: none; color: white;">Child Welfare</a>
+              <a href="education.html" style="text-decoration: none; color: white;">Education crowdfunding</a>
+              <a href="animal.html" style="text-decoration: none; color: white;">Animal crowdfunding</a>
+              <a href="other.html" style="text-decoration: none; color: white;">Others</a>
+        </div>
+      </div>
+
+      <div id="foot3">
+        <h2>Support</h2>
+        <div>
+        
+        <a href="team.html" style="text-decoration: none; color: white; ;">Team CareCatalyst</a>
+        <a href="howitworks.html" style="text-decoration: none; color: white; ;">What is Crowfunding?</a>
+  
+        </div>
+        
+      </div>
+      <div id="foot4">
+        <h2>About Us</h2>
+        <div>
+          <p>CareCatalyst is a dedicated fundraising organization that empowers individuals and communities to drive positive change.
+          CareCatalyst ignites positive change by empowering communities and donors. 
+          We connect passionate individuals with worthy causes, fueling impactful initiatives through strategic fundraising.
+          Together, we turn visions into realities, leaving a lasting impact on those who need it most.</p>
+        </div>
+      </div>
+     
+      
+    </div>
+    
+    <div id="bottomfoot">
+    
+        <img src="	https://ketto.gumlet.io/assets/images/homepage/footer-secured-card.png?w=457&dpr=2.0" alt="">
+     
+
+    </div>
+    </div>
+
+    <div id="affooter">
+      <p>Copyright © 2024 CareCatalyst Online Ventures Pvt Ltd. All Rights Reserved.
+        Terms of Use |
+        Privacy Policy |
+        AML Policy |
+        Use of cookies </p>
+    </div>
+
+    <div id="affooter2">
+      <p>Know more about CareCatalyst Crowdfunding Platform</p>
+    </div>
+
+
+  </footer>`
+}
+export default footer; 
